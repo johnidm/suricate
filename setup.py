d02095a4e7981a60c7d7004d7386d1ee3bc8d4ff
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2018-04-22 09:21:47 -0300

    Release 0.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='suricate',
     url='https://github.com/rougeth/rabisco/',
-    version='0.1',
+    version='0.0.1',
     author='Johni Douglas Marangon',
     author_email='johni.douglas.marangon@gmail.com',
     packages=find_packages(),
